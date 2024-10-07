edd026c0cab6d25104377c990236de7ba8b3680b
violet-wdream <2654887279@qq.com>
2024-10-08 00:01:21 +0800

    10-7

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -18,13 +18,15 @@
 
 // I AM NOT DONE
 
-pub struct ReportCard {
-    pub grade: f32,
+use std::fmt::Display;
+
+pub struct ReportCard<T: Display> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -48,17 +50,16 @@ mod tests {
         );
     }
 
-    #[test]
-    fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
-            student_name: "Gary Plotter".to_string(),
-            student_age: 11,
-        };
-        assert_eq!(
-            report_card.print(),
-            "Gary Plotter (11) - achieved a grade of A+"
-        );
+        #[test]
+        fn generate_alphabetic_report_card() {
+            let report_card = ReportCard {
+                grade: "A+",
+                student_name: "Gary Plotter".to_string(),
+                student_age: 11,
+            };
+            assert_eq!(
+                report_card.print(),
+                "Gary Plotter (11) - achieved a grade of A+"
+            );
+        }
     }
-}
